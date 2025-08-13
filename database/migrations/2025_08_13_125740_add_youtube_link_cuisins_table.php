612ec87216e61c2a5f6b916ccb1bff05a41444ab
Mkharzom <kharzom.mohammed.mal@gmail.com>
2025-08-13 13:01:38 +0800

    youtube_link

---
+++ database/migrations/2025_08_13_125740_add_youtube_link_cuisins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cuisines', function (Blueprint $table) {
+            $table->string('youtube_link')->nullable();
+        });
+
+        Schema::table('food_trucks', function (Blueprint $table) {
+            $table->string('youtube_link')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cuisines', function (Blueprint $table) {
+            $table->dropColumn('youtube_link');
+        });
+
+        Schema::table('food_trucks', function (Blueprint $table) {
+            $table->dropColumn('youtube_link');
+        });
+    }
+};
